7adfe4b2164c257394ad64c58d38eb07ed2c322e
Sahil kumawat <viratkumawat137@gmail.com>
2026-06-03 21:51:13 +0530

    Add files via upload

---
+++ funcation_example.js
@@ -0,0 +1,188 @@
+// JavaScript mein Function code ka ek block hota hai jo kisi kaam ko perform karta hai aur jab zarurat ho tab call kiya ja sakta hai.
+
+// 1.Function Declaration
+
+// function greet() {
+//   console.log("Hello Virat!");
+// }
+
+// greet();
+
+
+
+// 2.Parameters & Arguments  & regular funcations 
+
+// function add(a, b) {   // a aur b parameters hain
+//   console.log(a + b);
+// }
+
+// add(10, 20); // 10 aur 20 arguments hain
+
+
+// function greet  (a, b) {
+//   console.log(a + b);
+// }
+
+// greet(12,12);
+
+
+// 3.Return Statement
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// let result = multiply(5, 4);
+
+// console.log(result);
+
+
+// 4.Arrow Functions
+// const greet = () => {
+//   console.log("Hello!");
+// };
+
+// greet();
+
+
+
+// let a=(x,y) => {
+//     return x+y
+
+
+// }
+
+// console.log(a( 1,2));
+
+// 5.Callback Functions
+
+
+
+// let a=(x,y) => {
+//     return x+y
+
+// }
+
+// let b=(x,y) => {
+//     return x-y
+    
+// }
+
+// let c=(num1,num2) =>{
+//     return num1(2,3)+num2(3,4)
+// }
+// console.log(c(a,b));
+
+
+
+// let a = (x) => {
+//     return x
+// }
+
+// let b =(y) => {
+//     return y
+// }
+
+// let c=(num1,num2) =>{
+//     return num1("hello viraat") + num2("good bye...!")
+    
+// }
+
+// console.log(c(a,b));
+//  '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+
+// 1.
+// function myfunction (params){
+//     return 10
+
+// }
+
+// console.log(myfunction()+myfunction()+myfunction());
+
+
+
+
+// 2. 
+// function myfunction (params){
+//     return console.log("hii");
+
+// }
+
+// console.log(myfunction()+myfunction()+myfunction());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello, World!");
+
+
+// console.log("a"==="a");  
+
+// console.log("a"=="A");
+
+// console.log("1" == 1);
+
+// console.log(1 == "1");
+
+
+// console.log(1 === "1");
+
+// console.log("1" === 1);
+
+
+// console.log(1 === 1);
+
+// console.log([] === []);
+
+
+// let a = 10;
+// b = 10;
+
+// console.log( a === b ); 
+
+
+// yhaa pr a aur b dono alag alag memory location pe store honge to wo equal nhi honge 
+// let a = {};
+// b = {};
+
+// console.log( a === b );   
+
+
+// let sum = (a,b) => {
+//   return a+b;
+// }
+
+// let sub = (a,b) => {
+//   return a-b;
+// }
+
+// let mul = (a,b) => {
+//   return a*b;
+// }
+
+
+// let div = (a,b) => {
+//   return a/b;
+// }
+
+// let rem = (a,b) => {
+//   return a%b;
+// }
+
+
+// let all =(u,v,w,x,y) => {
+//     return u(10,5) + v(10,5) + w(10,5) + x(10,5) + y(10,5)
+// }
+
+// console.log(all(sum,sub,mul,div,rem));
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+function greet(name, callback) {
+//   console.log("Hello " + name);
+//   callback();
+// }
+
+// function bye() {
+//   console.log("Good Bye!");
+// }
+
+// greet("Virat", bye);
