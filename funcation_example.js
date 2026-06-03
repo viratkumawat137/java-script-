0320a47c3a01883df3510f3dc9c6eb8f5683a1f9
Sahil kumawat <viratkumawat137@gmail.com>
2026-06-04 03:22:48 +0530

    Uncomment JavaScript function examples and explanations

--- funcation_example.js
+++ funcation_example.js
@@ -36,7 +36,8 @@
 // console.log(result);
 
 
-// 4.Arrow Functions
+// 4.Arrow Functions.
+// 1.
 // const greet = () => {
 //   console.log("Hello!");
 // };
@@ -44,7 +45,7 @@
 // greet();
 
 
-
+// // 2.
 // let a=(x,y) => {
 //     return x+y
 
@@ -53,9 +54,9 @@
 
 // console.log(a( 1,2));
 
+
+
 // 5.Callback Functions
-
-
 
 // let a=(x,y) => {
 //     return x+y
@@ -73,7 +74,7 @@
 // console.log(c(a,b));
 
 
-
+// 2.
 // let a = (x) => {
 //     return x
 // }
@@ -88,6 +89,9 @@
 // }
 
 // console.log(c(a,b));
+
+
+
 //  '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 
@@ -186,3 +190,163 @@
 
 // console.log(all(sum,sub,mul,div,rem));
 
+
+
+
+
+
+
+
+// sssssssssssssssssssssssssssssssssssssssssssssssssssss   04-06-2026        sssssssssssssssssssssssssssssssssssssssssssssss
+
+// ifelse
+// elseif
+
+// let age = 15
+// if (age >= 18) {
+//     console.log("you can vote");
+
+// } else {
+//     console.log("you can't vote");
+
+// }
+
+// let time = 21
+// if (time >= 1 && time < 12) {
+//     console.log("Good morning");
+// } else if (time >= 12 && time < 17) {
+//     console.log("Good Afternoon");
+// } else if (time >= 17 && time < 22) {
+//     console.log("good Evening");
+// } else {
+//     console.log("good Night");
+// }
+
+// let value =21
+
+
+// 1.check the division. => marks like 60%
+
+// let a = 12;
+
+// if (a >= 90) {
+//     console.log("topper");
+//     console.log("congratulations");
+// }
+// else if (a >= 80) {
+//     console.log("first division");
+// }
+// else if (a >= 70) {
+//     console.log("second division");
+// }
+// else if (a >= 60) {
+//     console.log("third division");
+// }
+// else if (a >= 30) {
+//     console.log("pass");
+// }
+// else {
+//     console.log("fail");
+// }
+
+
+
+// 2. check the digit, special char, alphabet
+// let a= "13" 
+// if (a >= "0" && a <= "9") {
+//     console.log("digit");
+
+// }
+
+// else if (a >= "a" && a <= "z"){
+//     console.log("alaphabate");
+// }
+
+
+// else if (a >= "A" && a <= "Z"){
+//     console.log("alaphabate");
+// }
+
+
+// else if (a >= "!" && a <= "/"){
+//     console.log("special character");
+// }
+
+
+
+// let a= "1" 
+// if (a >= 0 && a <= 9) {
+//     console.log("digit");
+
+// }
+
+// else if (a >= 'a' && a <= 'z'){
+//     console.log("alaphabate");
+// }
+
+
+// else if (a >= 'A' && a <= 'Z'){
+//     console.log("alaphabate");
+// }
+
+
+// else if (a >= '!' && a <= '/'){
+//     console.log("special character");
+// }
+
+
+
+// 3.check the vowel a , e , i ,o,u or consonant
+
+// let a = "s"
+// if ( a =="a" || a =="e" || a =="i" || a =="o" || a =="u" ) {
+//     console.log("vowel");
+// } else {
+//     console.log("consonant");
+// }
+
+
+//4. let data  ='a'.   check lower or uppercase  
+// let data = "A"
+// if (data >= "a" && data <= "z"){
+//     console.log("Lowercase");
+// }
+
+// else if (data >= "A"  && data <= "Z"){
+//     console.log("Uppercase");
+// }
+
+// else {
+// console.log("......................??");
+// }
+
+
+// 5.alphabate or non-alphabate
+// let a= "1" 
+// if (a >= 0 && a <= 9) {
+//     console.log("Non-alphabate");
+
+// }
+
+// else if (a >= 'a' && a <= 'z'){
+//     console.log("alaphabate");
+// }
+
+
+// else if (a >= 'A' && a <= 'Z'){
+//     console.log("alaphabate");
+// }
+
+
+// else if (a >= '!' && a <= '/'){
+//     console.log("Non-alphabate");
+// }
+
+
+// 6. even number or odd number
+// let a = 10;
+// if (a % 2 === 0) {
+//     console.log("even number");
+// } else {
+//     console.log("odd number");
+// }
