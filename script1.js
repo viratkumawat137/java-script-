6cb005f6611c9528e3b3f9c2670042ce8f387277
Sahil kumawat <viratkumawat137@gmail.com>
2026-08-03 10:00:01 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -2,6 +2,6 @@ const data = document.getElementById("btn");
 data.addEventListener("click",()  => {
     document.getElementById("heading").innerHTML="welcome uniqo"
 })       
-
+                                                               
 document.write("hello world")
 // console.log("hello world rr");
